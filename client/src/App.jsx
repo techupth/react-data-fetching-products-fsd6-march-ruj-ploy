afd1bde61765da296fe0b92b43ee0764a0f58b98
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-06-07 15:07:27 +0700

    feat:add loading message and error message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,27 @@ import axios from "axios";
 function App() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsloading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const fetchProductHandle = async () => {
-    const response = await axios.get("http://localhost:4001/products");
-    setProducts(response.data.data);
-    console.log(response.data.data);
+    setIsloading(true);
+    try {
+      const response = await axios.get("http://localhost:4001/products");
+      setProducts(response.data.data);
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsloading(false);
   };
 
   const deleteProductHandle = async (id) => {
-    setIsloading(true);
     await axios.delete(`http://localhost:4001/products/${id}`);
-    setIsloading(false);
+    fetchProductHandle();
   };
 
   useEffect(() => {
     fetchProductHandle();
-  }, [isLoading]);
+  }, []);
 
   return (
     <div className="App">
@@ -27,32 +32,37 @@ function App() {
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        {products.map((item, index) => {
-          return (
-            <div key={index} className="product">
-              <div className="product-preview">
-                <img
-                  src={item.image}
-                  alt="some product"
-                  width="350"
-                  height="350"
-                />
-              </div>
-              <div className="product-detail">
-                <h1>Product name: {item.name}</h1>
-                <h2>Product price: {item.price} Baht</h2>
-                <p>Product description: {item.description}</p>
-              </div>
+        {isError && <h1>Fail to fetch data</h1>}
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          products.map((item, index) => {
+            return (
+              <div key={index} className="product">
+                <div className="product-preview">
+                  <img
+                    src={item.image}
+                    alt="some product"
+                    width="350"
+                    height="350"
+                  />
+                </div>
+                <div className="product-detail">
+                  <h1>Product name: {item.name}</h1>
+                  <h2>Product price: {item.price} Baht</h2>
+                  <p>Product description: {item.description}</p>
+                </div>
 
-              <button
-                onClick={() => deleteProductHandle(item.id)}
-                className="delete-button"
-              >
-                x
-              </button>
-            </div>
-          );
-        })}
+                <button
+                  onClick={() => deleteProductHandle(item.id)}
+                  className="delete-button"
+                >
+                  x
+                </button>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
