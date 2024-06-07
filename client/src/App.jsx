3b3704f0ebadaf8bad2cddd115f76060a508adb0
nattawara <nattawara.jtk@gmail.com>
2024-06-07 18:04:25 +0700

    feat: add delete get and delete data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,59 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [productInfo, setProductInfo] = useState([]);
+  const getProductInfo = async () => {
+    const result = await axios.get("http://localhost:4001/products");
+    setProductInfo(result.data.data);
+  };
+  const deleteProductInfo = async (id, itemIndex) => {
+    await axios.delete(`http://localhost:4001/products/${id}`);
+    const newproductInfo = [...productInfo];
+    newproductInfo.splice(itemIndex, 1);
+    setProductInfo(newproductInfo);
+  };
+  useEffect(() => {
+    getProductInfo();
+  }, []);
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        <ul>
+          {productInfo.map((item, index) => {
+            return (
+              <>
+                <div className="product" key={item.id}>
+                  <div className="product-preview">
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      width="350"
+                      height="350"
+                    />
+                  </div>
+                  <div className="product-detail">
+                    <h1>Product name: {item.name}</h1>
+                    <h2>Product price: {item.price} Baht</h2>
+                    <p>Product description: {item.description}</p>
+                  </div>
 
-          <button className="delete-button">x</button>
-        </div>
+                  <button
+                    className="delete-button"
+                    onClick={() => deleteProductInfo(item.id, index)}
+                  >
+                    x
+                  </button>
+                </div>
+              </>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
